d83902190c262e21cb2b92ad0bc28712bd21180d
Abdulrahman <abood.taef@gmail.com>
2023-06-17 14:42:47 +0400

    Initial Commit

---
+++ Flag Raising/Flag_RaisingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_RaisingApp.swift
+//  Flag Raising
+//
+//  Created by Abdulrahman Atta on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Flag_RaisingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
